7bd8484c88f3abe0c133e82fd3f27ff3c1409afe
Daniel da Conceição Marques <daniel.marques@firstdecision.com.br>
2021-02-19 19:53:35 -0300

    testing unsing axios and mocha

--- node05/01/index.js
+++ node05/01/index.js
@@ -1,18 +1,35 @@
 const { get } = require("axios");
 
-const URL = `https://swapi.dev/api/people`;
+const URL = "http://restemployeesapi-com.umbler.net/employees/list"
 
-async function obterPessoas(nome) {
-  const url = `${URL}/?search=${nome}&format=json`;
-  const result = await get(url);
-  return result.data.results.map(mapearPessoas);
+async function getUser(id){
+  const url = `${URL}/${id}`
+  const result = await get(url)
+  return result.data
 }
 
-function mapearPessoas(item) {
+function mapIds(item){
   return {
-    nome: item.name,
-    peso: item.height,
-  };
+    name: item.name,
+    age: item.age
+  }
 }
 
-module.exports = { obterPessoas };
+module.exports = { getUser }
+
+// const URL = `https://swapi.dev/api/people`;
+
+// async function obterPessoas(nome) {
+//   const url = `${URL}/?search=${nome}&format=json`;
+//   const result = await get(url);
+//   return result.data.results.map(mapearPessoas);
+// }
+
+// function mapearPessoas(item) {
+//   return {
+//     nome: item.name,
+//     peso: item.height,
+//   };
+// }
+
+// module.exports = { obterPessoas };
--- node05/01/tests.js
+++ node05/01/tests.js
@@ -1,12 +1,26 @@
 const assert = require("assert");
-const { obterPessoas } = require("./index");
-const nock = require("nock"); // Para simular requisições
+const { getUser } = require("./index")
 
-describe("Star Wars API", function () {
-  it("Deve buscar o r2d2 com o formato correto", async () => {
-    const expected = [{ nome: "R2-D2", peso: "96" }];
-    const nomeBase = `r2-d2`;
-    const resultado = await obterPessoas(nomeBase);
+describe("Rest API Employees", function () {
+  it("Should get one user by the id given", async () => {
+    const idBase = 40;
+    const expected = {id:idBase, name: "Jhon Wick", age: 38, OccupationId: 15, createdAt: "2021-02-16T14:01:48.000Z", updatedAt: "2021-02-16T14:01:48.000Z"};
+    const resultado = await getUser(idBase);
     assert.deepStrictEqual(resultado, expected);
   });
 });
+
+
+// assert.ok(false)
+
+// const { obterPessoas } = require("./index");
+// const nock = require("nock"); // Para simular requisições
+
+// describe("Star Wars API", function () {
+//   it("Deve buscar o r2d2 com o formato correto", async () => {
+//     const expected = [{ nome: "R2-D2", peso: "96" }];
+//     const nomeBase = `r2-d2`;
+//     const resultado = await obterPessoas(nomeBase);
+//     assert.deepStrictEqual(resultado, expected);
+//   });
+// });
